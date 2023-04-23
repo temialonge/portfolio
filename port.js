9bcbe4c372932d803902c190a1931358d44f96bc
temialonge <temitopealonge6@gmail.com>
2023-04-23 14:44:36 +0100

    first commit

---
+++ port.js
@@ -0,0 +1,12 @@
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll", () =>
+{
+    let offset = window.scrollY;
+    // console.log('offset: ' + offset);
+    // console.log('offset * 0.7' + offset * 0.7);
+    parallax.style.backgroundPositionY = offset * 0.7 + 'px'
+    parallaxx.style.backgroundPositionY = offset * 0.7 + 'px'
+    parallaxxx.style.backgroundPositionY = offset * 0.7 + 'px'
+})
+
