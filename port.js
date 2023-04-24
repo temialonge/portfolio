c81ecaf55864a376ad971cd1f9dee65c7c06ee4d
temialonge <temitopealonge6@gmail.com>
2023-04-24 10:34:37 +0100

    five

--- port.js
+++ port.js
@@ -1,16 +1,16 @@
-// const parallax = document.getElementById("parallax");
+const parallax = document.getElementById("parallax");
 
-// window.addEventListener("scroll", () =>
-// {
-//     let offset = window.scrollY;
-//     // console.log('offset: ' + offset);
-//     // console.log('offset * 0.7' + offset * 0.7);
-//     parallax.style.backgroundPositionY = offset * 0.7 + 'px'
-//     parallaxx.style.backgroundPositionY = offset * 0.7 + 'px'
-//     parallaxxx.style.backgroundPositionY = offset * 0.7 + 'px'
+window.addEventListener("scroll", () =>
+{
+    let offset = window.scrollY;
+    // console.log('offset: ' + offset);
+    // console.log('offset * 0.7' + offset * 0.7);
+    parallax.style.backgroundPositionY = offset * 0.7 + 'px'
+    parallaxx.style.backgroundPositionY = offset * 0.7 + 'px'
+    parallaxxx.style.backgroundPositionY = offset * 0.7 + 'px'
 
    
-// })
+})
 // if ($(window).width() < 640) {
 //  parallax.style.backgroundPositionY = `${offset}px`
    
